dd5fd120b36bb5234439cc42a441acef1e875e74
Nevres <nevres_muratovic@hotmail.com>
2022-07-20 00:36:46 +0200

    deleting and editing posts

--- home.js
+++ home.js
@@ -2,8 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
 import { getAuth, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
 import { getDatabase, ref, set, onValue, child, get, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
-import { collection, setDoc, query, where, getDocs, getFirestore, doc, onSnapshot, collectionGroup, addDoc } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js";
-import { getStorage, ref as sref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-storage.js";
+import { collection, deleteField, updateDoc, deleteDoc, orderBy, setDoc, query, where, getDoc, getDocs, getFirestore, doc, onSnapshot, collectionGroup, addDoc } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js";
+import { getStorage, deleteObject, listAll, ref as sref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,9 +26,7 @@ const user = auth.currentUser;
 const db = getFirestore();
 const storage = getStorage();
 var ms = (new Date()).getMilliseconds();
-// -----------------------------------------------------------geting name-----------------------------------------------------------
 
-// ----------------------------------------------------------------------------------------------------------------------------------------
 auth.onAuthStateChanged(user => {
   if (user !== null) {
     const full_name = user.full_name;
@@ -60,8 +58,6 @@ auth.onAuthStateChanged(user => {
     }
 
     reader.onload = function () {
-      // var posted_img = document.createElement('img')
-      // posted_img.src = reader.result;
       display_name.style.display = 'flex'
     }
 
@@ -86,7 +82,6 @@ auth.onAuthStateChanged(user => {
         onValue(RTdata, (snapshot) => {
           const real_data = snapshot.val();
           var user_name = real_data.full_name;
-
           let post_field = document.getElementById('post_input').value
           // Time format
           var timestamp = new Date();
@@ -97,17 +92,12 @@ auth.onAuthStateChanged(user => {
           var m = timestamp.getMinutes();
           var s = timestamp.getSeconds();
           timestamp = dd + '.' + mm + '.' + yyyy + ' | ' + h + ':' + m + ' :' + s;
-          // Data
-          // const input_data = {
-          //   text: post_field,
-          //   posted_at: timestamp,
-          //   user_uid: uid
-          // }
           const docRef = addDoc(collection(db, `users/${uid}/posts`), {
             text: post_field,
             posted_at: timestamp,
             user_uid: uid,
-            full_name: user_name
+            full_name: user_name,
+            m_seconds: Date.now()
           })
           let clear_field = document.getElementById('post_input')
           clear_field.value = '';
@@ -115,8 +105,6 @@ auth.onAuthStateChanged(user => {
       } else {
 
         // ----------------------------With Image----------------------------
-
-
         var imgToUpload = files[0];
 
         var ImgName = name_box.value
@@ -137,9 +125,6 @@ auth.onAuthStateChanged(user => {
             upload_message.innerHTML = 'Uploading. . .'
           } else {
             upload_message.innerHTML = '';
-
-            // variables
-
           }
 
         },
@@ -163,25 +148,21 @@ auth.onAuthStateChanged(user => {
                 var m = timestamp.getMinutes();
                 var s = timestamp.getSeconds();
                 timestamp = dd + '.' + mm + '.' + yyyy + ' | ' + h + ':' + m + ' :' + s;
-                // Data
-                // const input_data = {
-                //   text: post_field,
-                //   posted_at: timestamp,
-                //   user_uid: uid,
-                //   download_link: DownloadURL
-                // }
+
                 const docRef = addDoc(collection(db, `users/${uid}/posts`), {
                   text: post_field,
                   posted_at: timestamp,
                   user_uid: uid,
                   download_link: DownloadURL,
-                  full_name: user_name
+                  full_name: user_name,
+                  m_seconds: Date.now()
                 })
                 display_name.style.display = 'none'
                 upload_progress.innerHTML = "";
                 let clear_field = document.getElementById('post_input')
                 clear_field.value = '';
                 // name_box.value = '';
+                // document.location.reload()
               })
             });
           }
@@ -192,18 +173,36 @@ auth.onAuthStateChanged(user => {
     }
 
     document.getElementById('post_button').addEventListener('click', InsertData)
+    // ------------------------------------get data realtime-------------------------------
+
+    // async function getDataRealtime() {
+    //   // const rtPosts = sref(storage, 'users');
+    //   const posts = query(collectionGroup(db, 'posts'), orderBy('m_seconds', 'desc'));
+    //   const querySnapshot = await getDocs(posts);
+    //   console.log(querySnapshot)
+
+    // }
+    // getDataRealtime();
+
+
+
+
+
+
+
+
+
+
 
     // ---------------------------------Load all posts---------------------------------
 
     async function getAllPosts() {
-      const posts = collectionGroup(db, 'posts');
+      const posts = query(collectionGroup(db, 'posts'), orderBy('m_seconds', 'desc'));
+      // const posts = (collectionGroup(db, 'posts'));
       const querySnapshot = await getDocs(posts);
       querySnapshot.forEach((doc) => {
 
-        const RTdata = ref(database, 'users/' + user.uid);
-        onValue(RTdata, (snapshot) => {
-          const real_data = snapshot.val();
-        })
+
 
         var download_link = doc.data().download_link
         var posted_at = doc.data().posted_at
@@ -212,7 +211,7 @@ auth.onAuthStateChanged(user => {
         var full_name = doc.data().full_name
 
         var cardContainer = document.querySelector('.home-container');
-        // Create DIV
+        // Create Elements
         var new_content_card = document.createElement('div');
 
         var new_content_name = document.createElement('div');
@@ -230,6 +229,13 @@ auth.onAuthStateChanged(user => {
         var new_posted_at = document.createElement('p');
 
         var new_icon = document.createElement('i');
+
+        var new_dd_menu = document.createElement('ul');
+        var new_edit_p = document.createElement('li');
+        var new_delete_p = document.createElement('li');
+        var new_hide_p = document.createElement('li');
+
+
 
         var new_text_box = document.createElement('p');
         var new_hashtag = document.createElement('p');
@@ -273,7 +279,7 @@ auth.onAuthStateChanged(user => {
 
         new_small_profile_pic.classList.add('small_profile_pic');
         new_name_text.classList.add('name_text');
-        new_options_icon.classList.add('options_icon');
+        new_options_icon.classList.add('options_icon', 'dd_active');
 
 
         new_h4.classList.add('h4');
@@ -282,6 +288,16 @@ auth.onAuthStateChanged(user => {
         new_posted_at.innerHTML = `${posted_at}`;
 
         new_icon.classList.add('fa-solid', 'fa-xl', 'fa-ellipsis');
+
+        new_dd_menu.classList.add('dd_menu');
+        new_edit_p.classList.add('edit_p');
+        new_delete_p.classList.add('delete_p');
+        new_hide_p.classList.add('hide_p');
+        new_edit_p.innerHTML = ('Edit post');
+        new_delete_p.innerHTML = ('Delete post');
+        new_hide_p.innerHTML = ('Hide post');
+
+
 
         new_text_box.classList.add('text_box');
         new_text_box.innerHTML = `${text}`;
@@ -340,6 +356,13 @@ auth.onAuthStateChanged(user => {
 
         new_options_icon.appendChild(new_icon);
 
+        new_content_text.appendChild(new_dd_menu);
+        new_dd_menu.appendChild(new_edit_p);
+        new_dd_menu.appendChild(new_delete_p);
+        new_dd_menu.appendChild(new_hide_p);
+
+
+
         new_content_text.appendChild(new_text_box);
         new_content_text.appendChild(new_hashtag);
 
@@ -369,14 +392,224 @@ auth.onAuthStateChanged(user => {
         new_like_button.appendChild(new_like_icon_button);
         new_coment_button.appendChild(new_coment_icon_button);
         new_share_button.appendChild(new_share_icon_button);
+
+        var divs = document.getElementsByClassName('to_do');
+
+        for (let i = 0; i < divs.length; i++) {
+          divs[i].addEventListener("click", mousePosition);
+        }
       });
+      // ----------------DROPDOWN MENU HERE-----------------
+      var item = document.getElementsByClassName('options_icon');
+      Array.prototype.forEach.call(item, function (element) {
+        element.addEventListener('click', function () {
+
+
+
+          var dd = this
+          var menu = this.parentNode.nextElementSibling.firstChild;
+          if (dd.classList.contains('dd_active')) {
+            var menu = this.parentNode.nextElementSibling.firstChild;
+            var edit_menu = this.parentNode.nextElementSibling.firstChild.firstChild;
+            var delete_menu = this.parentNode.nextElementSibling.firstChild.firstChild.nextElementSibling;
+            var name_on_post = this.parentNode.firstChild.nextElementSibling.firstChild.innerHTML;
+            menu.style.clipPath = 'circle(100%)'
+            menu.style.transform = 'translate(-30px, 13px)'
+            dd.classList.remove('dd_active')
+
+            const RTdata = ref(database, 'users/' + user.uid);
+            onValue(RTdata, (snapshot) => {
+              const real_data = snapshot.val();
+              const current_name = real_data.full_name
+              if (current_name != name_on_post) {
+                delete_menu.style.display = 'none'
+                edit_menu.style.display = 'none'
+              }
+            })
+
+          }
+          else {
+            var menu = this.parentNode.nextElementSibling.firstChild;
+            menu.style.clipPath = 'circle(0%)'
+            menu.style.transform = 'translate(27px, -50px)'
+            dd.classList.add('dd_active')
+
+          }
+          document.onmouseup = (e) => {
+            var menu = this.parentNode.nextElementSibling.firstChild;
+            var edit_menu = this.parentNode.nextElementSibling.firstChild.firstChild;
+            var delete_menu = this.parentNode.nextElementSibling.firstChild.firstChild.nextElementSibling;
+            var hide_menu = this.parentNode.nextElementSibling.firstChild.firstChild.nextElementSibling.nextElementSibling;
+            if (e.target !== edit_menu && e.target !== hide_menu && e.target !== delete_menu) {
+              // var menu = this.parentNode.nextElementSibling.firstChild;
+              menu.style.clipPath = 'circle(0%)'
+              menu.style.transform = 'translate(27px, -50px)'
+              dd.classList.add('dd_active')
+            }
+          }
+        })
+      });
+      // --------------------------------------------------------UNFOCUS---------------------------------------------------------------------
+      // Array.prototype.forEach.call(item, function (element) {
+      //   element.addEventListener('blur', function () {
+      //     var menu = this.parentNode.nextElementSibling.firstChild;
+      //     menu.style.clipPath = 'circle(0%)'
+      //     menu.style.transform = 'translate(27px, -50px)'
+      //     dd.classList.add('dd_active')
+      //     console.log('blur in effect')
+      //   });
+      // })
+
+      // ------------------------------------------------------DELETE POST---------------------------------------------------------------
+      var item = document.getElementsByClassName('delete_p');
+      Array.prototype.forEach.call(item, function (element) {
+        element.addEventListener('click', deleteTest)
+      });
+      async function deleteTest() {
+        const timestamp = this.parentNode.parentNode.parentNode.firstChild.firstChild.nextElementSibling.firstChild.nextElementSibling.innerHTML
+        const refference = query(collection(db, `users/${uid}/posts`), where('posted_at', '==', timestamp));
+        const docSnap = await getDocs(refference)
+        docSnap.forEach(async (docmnt) => {
+          var doc_path = docmnt.ref.path.split("/")[3]
+          const post_path = doc(db, `users/${uid}/posts`, doc_path)
+          if (docmnt.exists()) {
+            await deleteDoc(post_path)
+              .then(() => {
+                document.location.reload()
+              })
+              .catch(() => {
+
+
+
+                console.log('something went wrong')
+              })
+          } else {
+            console.log(`Document doesn't exist`)
+          }
+        })
+      }
+      // ------------------------------------------------------Edit POST---------------------------------------------------------------
+      var item = document.getElementsByClassName('edit_p');
+      Array.prototype.forEach.call(item, function (element) {
+        element.addEventListener('click', editPost)
+      });
+      // function
+      async function editPost() {
+        scroll(0, 0);
+        var post_button = document.querySelector('.post_button');
+        var edit_button = document.querySelector('.edit_button');
+        post_button.style.display = 'none'
+        edit_button.style.display = 'block'
+
+        var post_div = document.querySelector('.post_card');
+        post_div.style.background = 'rgba(173, 51, 13, 0.4)'
+        let post_field = document.getElementById('post_input');
+        const timestamp = this.parentNode.parentNode.parentNode.firstChild.firstChild.nextElementSibling.firstChild.nextElementSibling.innerHTML
+
+        const refference = query(collection(db, `users/${uid}/posts`), where('posted_at', '==', timestamp));
+        const docSnap = await getDocs(refference)
+
+        docSnap.forEach(async (docmnt) => {
+          var doc_path = docmnt.ref.path.split("/")[3]
+          const post_path = doc(db, `users/${uid}/posts`, doc_path)
+          post_field.value = docmnt.data().text;
+        })
+        document.querySelector('.edit_button').addEventListener('click', function () {
+          docSnap.forEach(async (docmnt) => {
+            var doc_path = docmnt.ref.path.split("/")[3]
+            const post_path = doc(db, `users/${uid}/posts`, doc_path)
+            console.log(docmnt.data().text)
+            console.log(name_box)
+            if (name_box.value === '') {
+              await setDoc(
+                post_path, {
+                text: post_field.value,
+                posted_at: docmnt.data().posted_at,
+                user_uid: docmnt.data().user_uid,
+                full_name: docmnt.data().full_name,
+                m_seconds: docmnt.data().m_seconds
+              }
+              )
+                .then(() => {
+                  console.log('Post edited');
+                })
+                .catch(() => {
+                  console.log('something went wrong')
+                })
+              document.location.reload()
+            } else {
+              var imgToUpload = files[0];
+              var ImgName = name_box.value
+              const metaData = {
+                contentType: imgToUpload.type
+              }
+              const storage = getStorage();
+              const storageRef = sref(storage, "images/" + ImgName);
+              const uploadTask = uploadBytesResumable(storageRef, imgToUpload, metaData);
+              uploadTask.on('state-changed', (snapshot) => {
+                var upload_message = document.querySelector('.upload_message');
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                upload_progress.innerHTML = progress + "%";
+                if (progress != 100) {
+                  upload_message.innerHTML = 'Uploading. . .'
+                } else {
+                  upload_message.innerHTML = '';
+                }
+              },
+                (error) => {
+                  alert("error: image not uploaded");
+                },
+                () => {
+                  getDownloadURL(uploadTask.snapshot.ref).then((DownloadURL) => {
+                    const RTdata = ref(database, 'users/' + user.uid);
+                    onValue(RTdata, (snapshot) => {
+                      let post_field = document.getElementById('post_input');
+                      docSnap.forEach(async (docmnt) => {
+                        console.log(docmnt.data().text)
+                        const real_data = snapshot.val();
+                        var user_name = real_data.full_name;
+                        let post_field = document.getElementById('post_input')
+
+                        console.log(docmnt.data().posted_at)
+                        console.log(post_field.value)
+                        console.log(docmnt.data().user_uid)
+                        console.log(DownloadURL)
+                        console.log(docmnt.data().full_name)
+                        console.log(docmnt.data().m_seconds)
+
+                        await setDoc(
+                          post_path, {
+                          text: post_field.value,
+                          posted_at: docmnt.data().posted_at,
+                          user_uid: docmnt.data().user_uid,
+                          download_link: DownloadURL,
+                          full_name: docmnt.data().full_name,
+                          m_seconds: docmnt.data().m_seconds
+                        }
+                        )
+                        display_name.style.display = 'none'
+                        upload_progress.innerHTML = "";
+                        let clear_field = document.getElementById('post_input')
+                        clear_field.value = '';
+                        // name_box.value = '';
+                        document.location.reload();
+                      })
+                    })
+                    console.log(DownloadURL)
+                  });
+                }
+              );
+            };
+          })
+        })
+      }
+      // -------------------------------------------------------------------------------------------------------------------------------------------
     }
-    getAllPosts();
+    window.onload = getAllPosts();
   } else {
     window.open("/index.html", "_self");
   }
 })
-
 // ------------------------Sign Out----------------------------
 document.getElementById('sign_out').addEventListener('click', function () {
   signOut(auth).then(() => {
@@ -385,7 +618,6 @@ document.getElementById('sign_out').addEventListener('click', function () {
     alert('something went south');
   });
 })
-
 
 // Weather Widget
 !function (d, s, id) {
@@ -425,3 +657,4 @@ var divs = document.getElementsByClassName('to_do');
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener("click", mousePosition);
 }
+
